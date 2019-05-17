62e807ad4097d8e5b613d530169e69a89cb1c31a
Matt Casey <ccasey@ggc.edu>
2019-05-16 21:32:41 -0400

    First commit

---
+++ app/src/main/java/com/bignerdranch/mcasey/beatbox/BeatBoxActivity.java
@@ -0,0 +1,12 @@
+package com.bignerdranch.mcasey.beatbox;
+
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
